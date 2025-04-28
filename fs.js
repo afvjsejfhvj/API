0dbcc482189b177860ab3a245615ab4895a44835
MATEUS MULLER BISCARO <mateus_biscaro@estudante.sesisenai.org.br>
2025-04-28 10:14:20 -0300

    post, delete

--- fs.js
+++ fs.js
@@ -1,16 +1,16 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-// //ler arquivo
-// console.log('antes')
-// try{
-//     const data = fs.readFile('texte.txt', 'utf-8')
-//         console.log(durante)
+// // //ler arquivo
+// // console.log('antes')
+// // try{
+// //     const data = fs.readFile('texte.txt', 'utf-8')
+// //         console.log(durante)
     
-// }catch (e){
-//     console.log(e)
-// }
-// console.log('depois')
+// // }catch (e){
+// //     console.log(e)
+// // }
+// // console.log('depois')
 
 
-const texto = 'texto 12345'
-fs.appendFile('./texto.txt', texto, (err)=>{ if(err){console.log(err)}})
+// const texto = 'texto 12345'
+// fs.appendFile('./texto.txt', texto, (err)=>{ if(err){console.log(err)}})
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const express = require('express') //importar modulo express do npm 
 const fs = require('fs')
 const app = express() // inicializa servidor express e salva na variavel app
@@ -6,7 +7,7 @@ app.use(express.json()) // fala para o servidor que vai receber dados em json
 
 //criar minhas rotas
 app.get('/aulas', (req, res)=>{
-    fs.readFile('bancodedados.json', 'utf-8', (err, data)=> {
+    fs.readFile('./bancodedados.json', 'utf-8', (err, data)=> {
         if(err){
             res.status(500).json({msg:"erro no servidor"})
         }
@@ -18,12 +19,12 @@ app.get('/aulas', (req, res)=>{
 
 app.get('/aulas/:id', (req, res)=>{ //procurar usuario pelo id
     const id = req.params.id //pegando o id da url
-    fs.readFile('bancodedados.json', 'utf-8', (err, data)=> { //abrindo o aquivo banco de dados e lendo ele no formato utf-8
+    fs.readFile('./bancodedados.json', 'utf-8', (err, data)=> { //abrindo o aquivo banco de dados e lendo ele no formato utf-8
         if(err){ //testa se houve erro
             res.status(500).json({msg:"erro no servidor"})
         } //em caso de erro, DEVOLVA CODIGO 500 para o usuario
         const aulas = JSON.parse(data) // converte o arquivo texto -> json
-        const aula = aulas.find(aula.id == id) // procura na array de usuario e compara o seu id com o id procurado
+        const aula = aulas.find(aulas.id == id) // procura na array de usuario e compara o seu id com o id procurado
         if(aula){ // se achar o usuario
             res.status(200).json(aula) //retorna msg de sucesso
         }
@@ -35,41 +36,72 @@ app.post('/aulas', (req, res)=>{
     const dados = req.body
     fs.readFile('bancodedados.json', 'utf-8', (err, data)=> {
         if(err){
-            res.status(500).json({msg:"erro no servidor"})
-        }
-        const aulas = JSON.parse(data)
+           res.status(500).json({msg:"erro no servidor"})
+       }
+        const aulas = JSON.parse(data) //converte texto para json
         //cria id 
         dados['id'] = aulas.length +1
         //adicionar nova aula no array aulas 
         aulas.push(dados)
-        fs.writeFile(500).json({msg:"Erro no servidor"})
-    }) 
+        fs.writeFile('bancodedados.json', JSON.stringify(aulas), (err)=>{
+            if(err){
+                res.status(500).json({msg:"Erro no servidor"})
+            }
+            res.status(201).json(dados)
+        })
+    })
 
-    res.status(201).send(dados)
-}) 
+})
+
 
 app.put('/aulas/:id', (req, res) =>{
     //pegar id da rota
     const id = req.params.id
     // procurar o id do array
-    const usuarios = banco.find(user => user.id == id) 
-    if (!usuarios){
-        res.status(404).json({Msg:"usuario nao encontrado"})
-    } 
-    //modificar os campos
-    //atualizar o array
-    
-    res.send('ok')
+    fs.readFile('bancodedados.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).json({msg:"erro do servidor"})
+        }
+        const aulas = JSON.parse(data)
+        const aulaIndex = aulas.findIndex(aula => aula.id == id);
+        if(aulaIndex !== -1){
+            const dados = req.body;
+            for(key in dados){
+                aulas[aulaIndex][key] = dados[key]
+            }
+            fs.writeFile('bancodedados.json', JSON.stringify(aulas), (err) => 
+            {
+                if(err){
+                    res.status(500).json({msg:" erro do servidor"})
+                }
+                res.status(201).json(dados)
+            })
+        }else{
+            res.status(404).json({msg:'Aula nao encontrada'})
+        }
+    })
 })
+   
 app.delete('/aulas/:id', (req,res)=>{
     const id = req.params.id
-    const userIndex = banco.findIndex(user => user.id == id) 
-    if (userIndex === -1){
-        res.status(404).json({Msg:"usuario nao encontrado"})
-    }
-
-    banco.splice(userIndex, 1)
-    res.status(204).send()
+    fs.readFile('bancodedados.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).json({msg:"erro do servidor"})
+        }
+        const aulas = JSON.parse(data)
+        const aulaIndex = aulas.findIndex(aula => aula.id == id);
+        if(aulaIndex !== -1){
+            aulas.splice(aulaIndex, 1)
+            fs.writeFile( 'bancoDeDados. json', JSON. stringify(aulas), (err) => {
+            if (err) {
+                res.status(500).json({ msg: 'Erro no servidor' })
+            }
+                res.status (204).send()
+        })
+    } else {
+        res.status(404).json({msg: "Not Found"})
+        }
+    })
 })
-app.listen(PORT, ()=>{console.log('servidor online')}) // ()=>{'executa isso'} isso é um callback, coloca o servidor para ouvir 
 
+app.listen(PORT, ()=>{console.log('servidor online')}) // ()=>{'executa isso'} isso é um callback, coloca o servidor para ouvir 
